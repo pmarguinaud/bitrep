282cf05a58231f8ee72ea04f5f37caea3846c49b
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-01-16 11:45:19 +0000

    Reproducible functions in same executable (GPU/CPU)

--- br_main.F90
+++ br_main.F90
@@ -11,12 +11,12 @@ REAL (KIND=JPRB), PARAMETER :: ZATAN_X1 = -8._JPRB,  ZEXP_X1 = -5._JPRB,  ZLOG_X
 REAL (KIND=JPRB), PARAMETER :: ZATAN_X2 = +8._JPRB,  ZEXP_X2 = +5._JPRB,  ZLOG_X2 = 2000._JPRB,  ZSIN_X2 = +5._JPRB, ZASIN_X2 = +1._JPRB, ZCOS_X2 = +5._JPRB
 
 REAL (KIND=JPRB) :: ZATAN_X(NVAL),   ZEXP_X(NVAL),   ZLOG_X(NVAL),   ZSIN_X(NVAL),   ZASIN_X(NVAL),   ZCOS_X(NVAL)
-REAL (KIND=JPRB) :: ZATAN_Y(NVAL,2), ZEXP_Y(NVAL,2), ZLOG_Y(NVAL,2), ZSIN_Y(NVAL,2), ZASIN_Y(NVAL,2), ZCOS_Y(NVAL,2)
+REAL (KIND=JPRB) :: ZATAN_Y(NVAL,4), ZEXP_Y(NVAL,4), ZLOG_Y(NVAL,4), ZSIN_Y(NVAL,4), ZASIN_Y(NVAL,4), ZCOS_Y(NVAL,4)
 
 CHARACTER*8 :: CLARCH = ARCH
 
 INTEGER :: I, J
-CHARACTER, PARAMETER :: CLSUF (2) = ['i', 'b']
+CHARACTER, PARAMETER :: CLSUF (4) = ['i', 'b', 'I', 'B']
 REAL (KIND=JPRB) :: Z1, Z2
 
 ZATAN_X  = 0._JPRB; ZATAN_Y  = 0._JPRB
@@ -38,7 +38,7 @@ DO I = 1, NVAL
 ENDDO
 
 !$acc data copyin (ZATAN_X, ZEXP_X, ZLOG_X, ZSIN_X, ZASIN_X, ZCOS_X) &
-!$acc    & copyout (ZATAN_Y, ZEXP_Y, ZLOG_Y, ZSIN_Y, ZASIN_Y, ZCOS_Y)
+!$acc    & copyout (ZATAN_Y (:,1:2), ZEXP_Y (:,1:2), ZLOG_Y (:,1:2), ZSIN_Y (:,1:2), ZASIN_Y (:,1:2), ZCOS_Y (:,1:2))
 
 !$acc serial
 DO I = 1, NVAL
@@ -62,8 +62,25 @@ ENDDO
 
 !$acc end data
 
+DO I = 1, NVAL
+  ZATAN_Y (I,3) = ATAN (ZATAN_X (I))
+  ZEXP_Y  (I,3) = EXP  (ZEXP_X  (I))
+  ZLOG_Y  (I,3) = LOG  (ZLOG_X  (I))
+  ZSIN_Y  (I,3) = SIN  (ZSIN_X  (I))
+  ZASIN_Y (I,3) = ASIN (ZASIN_X (I))
+  ZCOS_Y  (I,3) = COS  (ZCOS_X  (I))
+ENDDO
 
-DO J = 1, 2
+DO I = 1, NVAL
+  ZATAN_Y (I,4) = BR_ATAN (ZATAN_X (I))
+  ZEXP_Y  (I,4) = BR_EXP  (ZEXP_X  (I))
+  ZLOG_Y  (I,4) = BR_LOG  (ZLOG_X  (I))
+  ZSIN_Y  (I,4) = BR_SIN  (ZSIN_X  (I))
+  ZASIN_Y (I,4) = BR_ASIN (ZASIN_X (I))
+  ZCOS_Y  (I,4) = BR_COS  (ZCOS_X  (I))
+ENDDO
+
+DO J = 1, 4
 
   OPEN (71, FILE="ZATAN."//TRIM (CLSUF (J))//"."//TRIM (CLARCH)//".dat", FORM="FORMATTED")
   OPEN (72, FILE="ZEXP_."//TRIM (CLSUF (J))//"."//TRIM (CLARCH)//".dat", FORM="FORMATTED")
--- check.sh
+++ check.sh
@@ -4,4 +4,5 @@ for x in SIN_ COS_ ASIN ATAN EXP_ LOG_
 do
   echo "==> $x <=="
   diff Z$x.b.gpu.dat Z$x.b.cpu.dat
+  diff Z$x.b.gpu.dat Z$x.B.gpu.dat
 done
